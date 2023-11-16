b9358ae1dfa974a457f6ffe5564d0ea05263aab8
Andrew-Moskalew <Lorenzo4474@gmail.com>
2023-11-17 00:13:28 +0300

    add OzbyPage.java class

---
+++ src/main/java/by/itacademy/moskalew/OzbyPage.java
@@ -0,0 +1,11 @@
+package by.itacademy.moskalew;
+
+public class OzbyPage {
+    public String buttonLogin = "//*[@id=\"top-page\"]/header/div[1]/div/div[1]/a";
+    public String buttonLoginOptionEmail = "//*[@id=\"loginFormLoginEmailLink\"]";
+    public String inputEmail = "//*[@id=\"loginForm\"]/div[2]/div[1]/div[1]/input";
+    public String inputPassword = "//*[@id=\"loginForm\"]/div[2]/div[1]/div[2]/input";
+    public String buttonEnter = "//*[@id=\"loginForm\"]/button";
+    public String errorMassage = "//*[@id=\"test\"]/div/div";
+    public String textEnter = "//*[@id=\"loginPopupIntro\"]";
+}
