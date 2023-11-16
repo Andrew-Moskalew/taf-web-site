1374ac168516358305c47dcfa3cee2f282f4c6ed
Andrew-Moskalew <Lorenzo4474@gmail.com>
2023-11-16 23:06:50 +0300

    rename class Website21vekbyPage.java to Vek21byPage.java

---
+++ src/main/java/by/itacademy/moskalew/Vek21byPage.java
@@ -0,0 +1,12 @@
+package by.itacademy.moskalew;
+
+public class Vek21byPage {
+    public String buttonAccount = "//*[@id=\"header\"]/div/div[3]/div/div[3]/div/div/div/button/span";
+    public String buttonLogin = "//*[@id=\"userToolsDropDown\"]/div/div[1]/div[2]/button";
+    public String inputEmail = "//*[@id=\"login-email\"]";
+    public String inputPassword = "//*[@id=\"login-password\"]";
+    public String errorMassageEmail = "//*[@id=\"default-:R2mm:-modal\"]/div/div/div[2]/div/div/form/div/div[1]/div[3]/span[2]";
+    public String errorMassagePassword = "//*[@id=\"default-:R2mm:-modal\"]/div/div/div[2]/div/div/form/div/div[2]/div[3]/span[2]";
+    public String buttonEnter = "//*[@id=\"default-:R2mm:-modal\"]/div/div/div[2]/div/div/form/div/div[3]/button";
+    public String buttonCookieReject = "//*[@id=\"modal-cookie\"]/div/div[2]/div/button[2]/div";
+}
